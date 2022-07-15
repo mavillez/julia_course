aea49285f2c7171915d871e3bc4b415157faa5ed
mavillez <60730834+mavillez@users.noreply.github.com>
2022-07-15 12:34:03 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Julia for Digital Humanities'
+copyright = '2022, M. Avillez'
+author = 'M.Avillez'
 
 release = '0.1'
 version = '0.1.0'
